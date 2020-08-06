0c97c1cae85446c61f8b6bdff0c5376200371d28
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-08-06 10:00:06 -0300

    sepolicy: Drop wifi-ext
    Change-Id: I418f9929e72b0d457ca99496a7409eae6ff587c5

--- sepolicy.mk
+++ sepolicy.mk
@@ -28,4 +28,3 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy-legacy-um/private
 
 -include device/cherish/sepolicy/qcom/sepolicy.mk
--include device/cherish/sepolicy/wifi-ext/sepolicy.mk
